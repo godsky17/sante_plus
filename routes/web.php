1ca73f7b309e150feb2886389167154f8d765a45
godsky17 <christiangauthe2@gmail.com>
2025-06-19 18:31:26 +0100

    Mise a jour des lien dans le composant header

--- routes/web.php
+++ routes/web.php
@@ -3,42 +3,46 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view("welcome");
+});
+
+Route::get('/accueil', function () {
     return view("index");
-});
+})->name('accueil');
 
 
-Route::get('/connexion', function () {
+Route::get('/auth/connexion', function () {
     return view("auth.connexion");
-});
+})->name('auth.connexion');
 
-Route::get('/inscription', function () {
+Route::get('/auth/inscription', function () {
     return view("auth.inscription");
-});
+})->name('auth.inscription');
 
-Route::get('/inscription/qui', function () {
+Route::get('/appointments/inscription/qui', function () {
     return view("auth.inscription_part_2");
-});
+})->name('appointments.inscription');
 
-Route::get('/symptomes', function () {
+Route::get('/appointments/symptomes', function () {
     return view("appointments.symptomes");
-});
+})->name('appointments.symptomes');
 
-Route::get('/specialistes', function () {
+Route::get('/appointments/specialistes', function () {
     return view("appointments.specialistes");
-});
+})->name('appointments.specialiste');
 
 
 Route::get('/appointments/remerciement', function () {
     return view("appointments.remerciement_rdv");
-});
+})->name('appointments.remerciement_rdv');
 
 Route::get('/appointments/docteur', function () {
     return view("appointments.profil_docteur");
-});
+})->name('appointments.profil_docteur');
 
 Route::get('/appointments/docteurs', function () {
     return view("appointments.docteurs");
-});
+})->name('appointments.docteurs');
 
 
 
